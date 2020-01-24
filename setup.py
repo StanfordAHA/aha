d82f69ffa0e413e3db71220fdc8bab8d784e4863
Teguh Hofstee <teguhhofstee@gmail.com>
2020-01-23 23:22:33 -0800

    move setup call before installing modules

--- setup.py
+++ setup.py
@@ -4,6 +4,24 @@ import subprocess
 import sys
 import os
 
+setup(
+    name='aha',
+    author='Teguh Hofstee',
+    url='https://github.com/hofstee/aha',
+
+    python_requires='>=3.7',
+    install_requires = [
+        'genesis2',
+        'networkx',
+        'packaging',
+        'pydot',
+    ],
+
+    entry_points = {
+        'console_scripts': ['aha=aha:main'],
+    },
+)
+
 def install(package):
     if os.path.exists(os.path.join('package', 'setup.py')):
         subprocess.check_call([sys.executable, "-m", "pip", "install", "-e", package])
@@ -35,20 +53,3 @@ for dep in deps.order_deps(modules):
 
 # TODO: `pip list` and ensure that all the above are pointing to sources
 
-setup(
-    name='aha',
-    author='Teguh Hofstee',
-    url='https://github.com/hofstee/aha',
-
-    python_requires='>=3.7',
-    install_requires = [
-        'genesis2',
-        'networkx',
-        'packaging',
-        'pydot',
-    ],
-
-    entry_points = {
-        'console_scripts': ['aha=aha:main'],
-    },
-)
