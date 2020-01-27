42d5bcdcc9e5a54361f078484128269a1e87c1be
Teguh Hofstee <teguhhofstee@gmail.com>
2020-01-27 15:36:31 -0800

    update setup.py

--- aha.py
+++ aha.py
@@ -1,9 +1,41 @@
 import argparse
+import docker
+import git
 import logging
 import subprocess
 import sys
 import os
 from pathlib import Path
+
+
+def in_docker():
+    return os.path.exists('/.dockerenv')
+
+
+def test():
+    aha_dir = os.path.dirname(os.path.abspath(__file__))
+    aha_repo = git.Repo(aha_dir)
+    aha_branch = aha_repo.head.ref
+
+    docker_tag = 'latest' if aha_branch == aha_repo.heads.master else str(aha_branch)
+
+    client = docker.from_env()
+
+    try:
+        image = client.images.get(f'stanfordaha/garnet:{docker_tag}')
+    except docker.errors.ImageNotFound:
+        logging.warning(f"Pulling 'stanfordaha/garnet:{docker_tag}' from registry...")
+        image = client.images.pull('stanfordaha/garnet', tag=docker_tag)
+
+    logging.info("Checking registry for updates...")
+    latest_data = client.images.get_registry_data('stanfordaha/garnet')
+
+    if image.id != latest_data.id:
+        logging.warning(f"An update for 'stanfordaha/garnet:{docker_tag}' is available.")
+        logging.warning(f"Run `docker pull stanfordaha/garnet:{docker_tag}` to update.")
+
+    print(client.containers.run(image, "echo hello world"))
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -20,9 +52,11 @@ def main():
 
 
     args, extra_args = parser.parse_known_args()
-    print(args)
 
     logging.basicConfig(level=min(args.verbose, args.debug))
+
+    if not in_docker():
+        test()
 
     if args.command == 'garnet':
         subprocess.call(
--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ import subprocess
 import sys
 import os
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-
 
 def install(package):
     if os.path.exists(os.path.join('package', 'setup.py')):
@@ -13,30 +11,51 @@ def install(package):
 
 # TODO: only install these if they're not present in `pip list`
 
-modules = {
-    'archipelago': 'archipelago',
-    'ast_tools': 'ast_tools',
-    'buffer-mapping': 'BufferMapping',
-    'canal': 'canal',
-    'coreir': 'pycoreir',
-    'cosa': 'cosa',
-    'fault': 'fault',
-    'garnet': 'garnet',
-    'gemstone': 'gemstone',
-    'hwtypes': 'hwtypes',
-    'kratos': 'kratos',
-    'lassen': 'lassen',
-    'magma-lang': 'magma',
-    'mantle': 'mantle',
-    'peak': 'peak',
-    'pycyclone': 'cgra_pnr/cyclone',
-    'pythunder': 'cgra_pnr/thunder',
-}
+install(os.path.join(os.getcwd(), 'ast_tools'))
+install(os.path.join(os.getcwd(), 'hwtypes'))
+install(os.path.join(os.getcwd(), 'pycoreir'))
+install(os.path.join(os.getcwd(), 'kratos'))
+install(os.path.join(os.getcwd(), 'magma'))
+install(os.path.join(os.getcwd(), 'cosa'))
+install(os.path.join(os.getcwd(), 'fault'))
+install(os.path.join(os.getcwd(), 'mantle'))
+install(os.path.join(os.getcwd(), 'canal'))
+install(os.path.join(os.getcwd(), 'gemstone'))
+install(os.path.join(os.getcwd(), 'peak'))
+install(os.path.join(os.getcwd(), 'lassen'))
+install(os.path.join(os.getcwd(), 'cgra_pnr/cyclone'))
+install(os.path.join(os.getcwd(), 'cgra_pnr/thunder'))
+install(os.path.join(os.getcwd(), 'archipelago'))
+
+# modules = {
+#     'archipelago': 'archipelago',
+#     'ast_tools': 'ast_tools',
+#     'buffer-mapping': 'BufferMapping',
+#     'canal': 'canal',
+#     'coreir': 'pycoreir',
+#     'cosa': 'cosa',
+#     'fault': 'fault',
+#     'garnet': 'garnet',
+#     'gemstone': 'gemstone',
+#     'hwtypes': 'hwtypes',
+#     'kratos': 'kratos',
+#     'lassen': 'lassen',
+#     'magma-lang': 'magma',
+#     'mantle': 'mantle',
+#     'peak': 'peak',
+#     'pycyclone': 'cgra_pnr/cyclone',
+#     'pythunder': 'cgra_pnr/thunder',
+# }
 
 
-import deps
-for dep in deps.order_deps(modules):
-    install(os.path.join(os.getcwd(), modules[dep]))
+# for dep in modules:
+#     print(dep)
+
+
+# import deps
+# for dep in deps.order_deps(modules):
+#     print(modules[dep])
+#     install(os.path.join(os.getcwd(), modules[dep]))
 
 # TODO: `pip list` and ensure that all the above are pointing to sources
 
@@ -47,7 +66,15 @@ setup(
 
     python_requires='>=3.7',
     install_requires = [
+        'docker',
         'genesis2',
+        'networkx',
+        'packaging',
+        'pydot',
+    ],
+    setup_requires = [
+        'networkx',
+        'packaging',
     ],
 
     entry_points = {
