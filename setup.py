7ca26e10d5e5a86f2a86347a28b00200729f13ce
Teguh Hofstee <teguhhofstee@gmail.com>
2020-01-28 16:28:26 -0800

    topologically sort dependencies

--- setup.py
+++ setup.py
@@ -11,51 +11,31 @@ def install(package):
 
 # TODO: only install these if they're not present in `pip list`
 
-install(os.path.join(os.getcwd(), 'ast_tools'))
-install(os.path.join(os.getcwd(), 'hwtypes'))
-install(os.path.join(os.getcwd(), 'pycoreir'))
-install(os.path.join(os.getcwd(), 'kratos'))
-install(os.path.join(os.getcwd(), 'magma'))
-install(os.path.join(os.getcwd(), 'cosa'))
-install(os.path.join(os.getcwd(), 'fault'))
-install(os.path.join(os.getcwd(), 'mantle'))
-install(os.path.join(os.getcwd(), 'canal'))
-install(os.path.join(os.getcwd(), 'gemstone'))
-install(os.path.join(os.getcwd(), 'peak'))
-install(os.path.join(os.getcwd(), 'lassen'))
-install(os.path.join(os.getcwd(), 'cgra_pnr/cyclone'))
-install(os.path.join(os.getcwd(), 'cgra_pnr/thunder'))
-install(os.path.join(os.getcwd(), 'archipelago'))
-
-# modules = {
-#     'archipelago': 'archipelago',
-#     'ast_tools': 'ast_tools',
-#     'buffer-mapping': 'BufferMapping',
-#     'canal': 'canal',
-#     'coreir': 'pycoreir',
-#     'cosa': 'cosa',
-#     'fault': 'fault',
-#     'garnet': 'garnet',
-#     'gemstone': 'gemstone',
-#     'hwtypes': 'hwtypes',
-#     'kratos': 'kratos',
-#     'lassen': 'lassen',
-#     'magma-lang': 'magma',
-#     'mantle': 'mantle',
-#     'peak': 'peak',
-#     'pycyclone': 'cgra_pnr/cyclone',
-#     'pythunder': 'cgra_pnr/thunder',
-# }
+modules = {
+    'archipelago': 'archipelago',
+    'ast_tools': 'ast_tools',
+    'buffer-mapping': 'BufferMapping',
+    'canal': 'canal',
+    'coreir': 'pycoreir',
+    'cosa': 'cosa',
+    'fault': 'fault',
+    'garnet': 'garnet',
+    'gemstone': 'gemstone',
+    'hwtypes': 'hwtypes',
+    'kratos': 'kratos',
+    'lassen': 'lassen',
+    'magma-lang': 'magma',
+    'mantle': 'mantle',
+    'peak': 'peak',
+    'pycyclone': 'cgra_pnr/cyclone',
+    'pythunder': 'cgra_pnr/thunder',
+}
 
 
-# for dep in modules:
-#     print(dep)
-
-
-# import deps
-# for dep in deps.order_deps(modules):
-#     print(modules[dep])
-#     install(os.path.join(os.getcwd(), modules[dep]))
+import deps
+for dep in deps.order_deps(modules):
+    print(modules[dep])
+    install(os.path.join(os.getcwd(), modules[dep]))
 
 # TODO: `pip list` and ensure that all the above are pointing to sources
 
