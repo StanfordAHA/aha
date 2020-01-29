7a7ef496f2dd2fd4d72d0f396ed34c0e567a338e
Teguh Hofstee <teguhhofstee@gmail.com>
2020-01-28 16:55:32 -0800

    move up setup call

--- setup.py
+++ setup.py
@@ -4,6 +4,31 @@ import sys
 import os
 
 
+setup(
+    name='aha',
+    author='Teguh Hofstee',
+    url='https://github.com/hofstee/aha',
+
+    python_requires='>=3.7',
+    install_requires = [
+        'docker',
+        'genesis2',
+        'gitpython',
+        'networkx',
+        'packaging',
+        'pydot',
+    ],
+    setup_requires = [
+        'networkx',
+        'packaging',
+    ],
+
+    entry_points = {
+        'console_scripts': ['aha=aha:main'],
+    },
+)
+
+
 def install(package):
     if os.path.exists(os.path.join(package, 'setup.py')):
         subprocess.check_call([sys.executable, "-m", "pip", "install", "-e", package])
@@ -38,27 +63,3 @@ for dep in deps.order_deps(modules):
     install(os.path.join(os.getcwd(), modules[dep]))
 
 # TODO: `pip list` and ensure that all the above are pointing to sources
-
-setup(
-    name='aha',
-    author='Teguh Hofstee',
-    url='https://github.com/hofstee/aha',
-
-    python_requires='>=3.7',
-    install_requires = [
-        'docker',
-        'genesis2',
-        'gitpython',
-        'networkx',
-        'packaging',
-        'pydot',
-    ],
-    setup_requires = [
-        'networkx',
-        'packaging',
-    ],
-
-    entry_points = {
-        'console_scripts': ['aha=aha:main'],
-    },
-)
