3090662872cef5a709bfe481b0cff605d67d6f2a
joyliu37 <joyliu37@sina.com>
2021-04-06 15:13:53 -0700

    migrate clockwork mapping from aha map to aha halide

--- aha/util/halide.py
+++ aha/util/halide.py
@@ -1,13 +1,14 @@
 import copy
 import os
+import shutil
 from pathlib import Path
 import subprocess
-import sys
 
 
 def add_subparser(subparser):
     parser = subparser.add_parser(Path(__file__).stem)
     parser.add_argument("app")
+    parser.add_argument("--sim", action='store_true')
     parser.set_defaults(dispatch=dispatch)
 
 
@@ -15,15 +16,34 @@ def dispatch(args, extra_args=None):
     args.app = Path(args.app)
     env = copy.deepcopy(os.environ)
     env["COREIR_DIR"] = str(args.aha_dir / "coreir")
+    env["COREIR_PATH"] = str(args.aha_dir / "coreir")
+    env["LAKE_PATH"] = str(args.aha_dir / "lake")
+    env["CLOCKWORK_PATH"] = str(args.aha_dir / "clockwork")
     halide_dir = args.aha_dir / "Halide-to-Hardware"
     app_dir = halide_dir / Path("apps/hardware_benchmarks") / args.app
+    env["LAKE_CONTROLLERS"] = str(app_dir / "bin")
+    env["LAKE_STREAM"] = str(app_dir / "bin")
 
-    # CoreIR Output
-    subprocess.check_call(
-        ["make", "-C", app_dir, "design-clockwork"],
-        cwd=args.aha_dir / "Halide-to-Hardware",
-        env=env,
-    )
+    app_name = args.app.name
+    run_sim = args.sim
+
+    if run_sim:
+        subprocess.check_call(
+            ["make", "-C", app_dir, "test-mem"],
+            cwd=args.aha_dir / "Halide-to-Hardware",
+            env=env,
+        )
+    else:
+        subprocess.check_call(
+            ["make", "-C", app_dir, "mem"],
+            cwd=args.aha_dir / "Halide-to-Hardware",
+            env=env,
+        )
+
+    #move to apps/bin
+    clkwrk_design = app_name +"/" + app_name + "_garnet.json"
+    if os.path.exists(str(app_dir /" bin/map_result"/ clkwrk_design)):
+        shutil.move(str(app_dir / "bin/map_result" / clkwrk_design), str(app_dir / "bin/design_top.json"))
 
     # Raw Images
     subprocess.check_call(
--- aha/util/map.py
+++ aha/util/map.py
@@ -1,6 +1,3 @@
-import copy
-import os
-import shutil
 from pathlib import Path
 import subprocess
 import sys
@@ -10,19 +7,11 @@ def add_subparser(subparser):
     parser = subparser.add_parser(Path(__file__).stem, add_help=False)
     parser.add_argument("app")
     parser.add_argument("--base", default=None, type=str)
-    parser.add_argument("--sim", action='store_true')
     parser.set_defaults(dispatch=dispatch)
 
 
 def dispatch(args, extra_args=None):
     args.app = Path(args.app)
-
-    env = copy.deepcopy(os.environ)
-    env["COREIR_DIR"] = str(args.aha_dir / "coreir")
-    env["COREIR_PATH"] = str(args.aha_dir / "coreir")
-    env["LAKE_PATH"] = str(args.aha_dir / "lake")
-    env["CLOCKWORK_PATH"] = str(args.aha_dir / "clockwork")
-    env["LAKE_CONTROLLERS"] = str(args.aha_dir / "clockwork")
 
     if args.base is None:
         app_dir = Path(
@@ -30,44 +19,21 @@ def dispatch(args, extra_args=None):
         )
     else:
         app_dir = (Path(args.base) / args.app).resolve()
-    env["LAKE_CONTROLLERS"] = str(app_dir / "bin")
-    env["LAKE_STREAM"] = str(app_dir / "bin")
 
-    app_name = args.app.name
+    map_args = [
+        "--no-pd",
+        "--interconnect-only",
+        "--input-app",
+        app_dir / "bin/design_top.json",
+        "--input-file",
+        app_dir / "bin/input.raw",
+        "--output-file",
+        app_dir / f"bin/{args.app.name}.bs",
+        "--gold-file",
+        app_dir / "bin/gold.raw",
+    ]
 
-    run_sim = args.sim
-    if run_sim:
-        subprocess.check_call(
-            ["make", "-C", app_dir, "test-mem"],
-            cwd=args.aha_dir / "Halide-to-Hardware",
-            env=env,
-        )
-    else:
-        subprocess.check_call(
-            ["make", "-C", app_dir, "mem"],
-            cwd=args.aha_dir / "Halide-to-Hardware",
-            env=env,
-        )
-
-        #move to apps/bin
-        clkwrk_design = app_name +"/" + app_name + "_garnet.json"
-        shutil.move(str(app_dir / "bin/map_result" / clkwrk_design), str(app_dir / "bin/design_top.json"))
-
-
-        map_args = [
-            "--no-pd",
-            "--interconnect-only",
-            "--input-app",
-            app_dir / "bin/design_top.json",
-            "--input-file",
-            app_dir / "bin/input.raw",
-            "--output-file",
-            app_dir / f"bin/{args.app.name}.bs",
-            "--gold-file",
-            app_dir / "bin/gold.raw",
-        ]
-
-        subprocess.check_call(
-            [sys.executable, "garnet.py"] + map_args + extra_args,
-            cwd=args.aha_dir / "garnet",
-        )
+    subprocess.check_call(
+        [sys.executable, "garnet.py"] + map_args + extra_args,
+        cwd=args.aha_dir / "garnet",
+    )
