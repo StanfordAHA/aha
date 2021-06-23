1d4c83cf0305be25b8f820afd20cd6d2eb67800f
Taeyoung Kong <kongty@stanford.edu>
2021-06-22 23:58:10 -0700

    Add python script to glb flow

--- aha/util/glb.py
+++ aha/util/glb.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import subprocess
+import sys
 import os
 
 
@@ -22,6 +23,15 @@ def dispatch(args, extra_args=None):
         # get the full path of the app
         arg_path = f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/{app}"
         app_args.append(f"+{arg_name}={arg_path}")
+        # generate meta_data.json file
+        subprocess.check_call(
+            [sys.executable,
+             f"../../hw_support/parse_design_meta.py",
+             "bin/design_meta_halide.json",
+             "--top", "bin/design_top.json",
+             "--place", "bin/design.place"],
+            cwd=arg_path
+        )
     app_args = " ".join(app_args)
     env["APP_ARGS"] = app_args
     # figure out garnet.v path
