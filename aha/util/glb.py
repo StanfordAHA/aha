88f57d40404392f13696b64939f46f34c345e4d5
Taeyoung Kong <kongty@stanford.edu>
2021-10-05 17:27:38 -0700

    Add no-parse and shuffle flag to aha glb

--- aha/util/glb.py
+++ aha/util/glb.py
@@ -10,6 +10,9 @@ def add_subparser(subparser):
     parser.add_argument("--width", type=int, default=16)
     parser.add_argument("--vcd", action="store_true")
     parser.add_argument("--vcd-glb", action="store_true")
+    parser.add_argument("--run", action="store_true")
+    parser.add_argument("--shuffle", action="store_true")
+    parser.add_argument("--no-parse", action="store_true")
     parser.set_defaults(dispatch=dispatch)
 
 
@@ -24,14 +27,33 @@ def dispatch(args, extra_args=None):
         arg_path = f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/{app}"
         app_args.append(f"+{arg_name}={arg_path}")
         # generate meta_data.json file
-        subprocess.check_call(
-            [sys.executable,
-             f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/hw_support/parse_design_meta.py",
-             "bin/design_meta_halide.json",
-             "--top", "bin/design_top.json",
-             "--place", "bin/design.place"],
-            cwd=arg_path
-        )
+        if not args.no_parse:
+            if args.shuffle:
+                subprocess.check_call(
+                    [sys.executable,
+                     f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/hw_support/shuffle.py",
+                     "design_top.json"],
+                    cwd=f"{arg_path}/bin"
+                )
+                subprocess.check_call(
+                    [sys.executable,
+                     f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/hw_support/parse_design_meta.py",
+                     "bin/design_meta_halide.json",
+                     "--top", "bin/design_top_reorder.json",
+                     "--place", "bin/design.place",
+                     "--shuffle"],
+                    cwd=arg_path
+                )
+            else:
+                subprocess.check_call(
+                    [sys.executable,
+                     f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/hw_support/parse_design_meta.py",
+                     "bin/design_meta_halide.json",
+                     "--top", "bin/design_top.json",
+                     "--place", "bin/design.place"],
+                    cwd=arg_path
+                )
+
     app_args = " ".join(app_args)
     env["APP_ARGS"] = app_args
     # figure out garnet.v path
@@ -44,8 +66,18 @@ def dispatch(args, extra_args=None):
     elif args.vcd_glb:
         env["RUN_ARGS"] = "-input shm_glb.tcl"
 
-    subprocess.check_call(
-        ["make", "sim"] + extra_args,
-        cwd=str(args.aha_dir / "garnet" / "tests" / "test_app"),
-        env=env
-    )
+    if args.shuffle:
+        env["LIB_ARGS"] = "-DSHUFFLE"
+
+    if args.run:
+        subprocess.check_call(
+            ["make", "run"] + extra_args,
+            cwd=str(args.aha_dir / "garnet" / "tests" / "test_app"),
+            env=env
+        )
+    else:
+        subprocess.check_call(
+            ["make", "sim"] + extra_args,
+            cwd=str(args.aha_dir / "garnet" / "tests" / "test_app"),
+            env=env
+        )
