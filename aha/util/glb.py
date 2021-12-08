aaf0800f5dda25ed949c239dcb3287bf8fe27b22
Taeyoung Kong <kongty@stanford.edu>
2021-12-08 14:53:38 -0800

    Move parse script to map

--- aha/util/glb.py
+++ aha/util/glb.py
@@ -10,7 +10,6 @@ def add_subparser(subparser):
     parser.add_argument("--vcd", action="store_true")
     parser.add_argument("--vcd-glb", action="store_true")
     parser.add_argument("--run", action="store_true")
-    parser.add_argument("--no-parse", action="store_true")
     parser.set_defaults(dispatch=dispatch)
 
 
@@ -24,16 +23,6 @@ def dispatch(args, extra_args=None):
         # get the full path of the app
         arg_path = f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/{app}"
         app_args.append(f"+{arg_name}={arg_path}")
-        # generate meta_data.json file
-        if not args.no_parse:
-            subprocess.check_call(
-                [sys.executable,
-                 f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/hw_support/parse_design_meta.py",
-                 "bin/design_meta_halide.json",
-                 "--top", "bin/design_top.json",
-                 "--place", "bin/design.place"],
-                cwd=arg_path
-            )
 
     app_args = " ".join(app_args)
     env["APP_ARGS"] = app_args
