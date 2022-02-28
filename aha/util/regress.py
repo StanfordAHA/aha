10045671ee5e1692d1c711f51f22bfab11e991ae
jack-melchert <56329001+jack-melchert@users.noreply.github.com>
2022-02-28 12:36:34 -0800

    Update regress.py

--- aha/util/regress.py
+++ aha/util/regress.py
@@ -200,10 +200,10 @@ def dispatch(args, extra_args=None):
     print(f"--- Running regression: {args.config}")
     info = []
     t = gen_garnet(width, height)
-    info.append(["apps/garnet", t])
+    info.append(["garnet", t])
 
     halide_gen_args = {}
-    halide_gen_args["gaussian"] = "mywidth=64 myunroll=14 schedule=1"
+    halide_gen_args["apps/gaussian"] = "mywidth=64 myunroll=14 schedule=1"
 
     for test in tests:
         if test in halide_gen_args:
