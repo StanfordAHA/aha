5a67bd207a7db3187f1432fa3996f461e72bb867
Jackson Melchert <melchert@stanford.edu>
2022-03-10 12:58:39 -0800

    Updated visualization

--- aha/util/sta.py
+++ aha/util/sta.py
@@ -7,7 +7,6 @@ import copy
 def add_subparser(subparser):
     parser = subparser.add_parser(Path(__file__).stem, add_help=False)
     parser.add_argument("app")
-    parser.add_argument("--visualize", action="store_true")
     parser.set_defaults(dispatch=dispatch)
 
 
@@ -23,9 +22,3 @@ def dispatch(args, extra_args=None):
         cwd=args.aha_dir / "archipelago/archipelago",
     )
 
-    if args.visualize:
-        subprocess.check_call(
-            [sys.executable, "visualize.py", "-a", app_dir]  + extra_args,
-            cwd=args.aha_dir / "archipelago/archipelago",
-        )
-       
