4ce65aa4c73e2769413bc8a5365d51a90e5788cd
root <melchert@stanford.edu>
2022-03-30 13:42:26 -0700

    Updated metamapper, clockwork, and aha utils

--- aha/util/regress.py
+++ aha/util/regress.py
@@ -49,7 +49,8 @@ def gen_garnet(width, height):
         str(width),
         "--height",
         str(height),
-        "--verilog"
+        "--verilog",
+        "--use_sim_sram"
         #"--interconnect-only"
     ])
     return time.time() - start
@@ -63,6 +64,7 @@ def run_test(testname, width, height):
     time_compile = time.time() - start
 
     print(f"--- {testname} - mapping")
+    os.environ["PIPELINED"] = '1'
     start = time.time()
     buildkite_call(
         ["aha", "map", testname, "--width", str(width), "--height", str(height)]
@@ -77,17 +79,23 @@ def run_test(testname, width, height):
     return time_compile, time_map, time_test
 
 
-def run_glb(testname, width, height):
+def run_glb(testname, width, height, test=''):
     app_path = "/aha/Halide-to-Hardware/apps/hardware_benchmarks/"+testname
     print(app_path)
-    subprocess.call(["make", "clean"], cwd=app_path)
-    print(f"--- {testname}")
-    print(f"--- {testname} - compiling")
+    try:
+        subprocess.call(["make", "clean"], cwd=app_path)
+    except: 
+        pass
+    if test == '':
+        test = testname
+    print(f"--- {test}")
+    print(f"--- {test} - compiling")
+    os.environ["PIPELINED"] = '1'
     start = time.time()
     buildkite_call(["aha", "halide", testname])
     time_compile = time.time() - start
 
-    print(f"--- {testname} - mapping")
+    print(f"--- {test} - mapping")
     start = time.time()
     my_env = {}
     #if testname == "apps/unsharp" or testname == "apps/camera_pipeline":
@@ -98,7 +106,7 @@ def run_glb(testname, width, height):
     )
     time_map = time.time() - start
 
-    print(f"--- {testname} - glb testing")
+    print(f"--- {test} - glb testing")
     start = time.time()
     buildkite_call(["aha", "glb", testname])
     time_test = time.time() - start
@@ -112,7 +120,8 @@ def dispatch(args, extra_args=None):
         glb_tests = [
             "apps/pointwise",
         ]
-        tests = [ ]
+        tests = []
+        resnet_tests = []
     elif args.config == "pr":
         width, height = 8, 8
         glb_tests = [
@@ -130,9 +139,10 @@ def dispatch(args, extra_args=None):
             # "handcrafted/pond_accum",
             # "handcrafted/pond_and_mem"
         ]
-        tests = [ ]
+        tests = []
+        resnet_tests = []
     elif args.config == "daily":
-        width, height = 16, 16
+        width, height = 32, 16
         tests = [
             # "apps/pointwise",
             # "tests/rom",
@@ -158,6 +168,11 @@ def dispatch(args, extra_args=None):
             "apps/gaussian",
             "apps/unsharp",
             "apps/resnet_layer_gen"
+        ]
+        resnet_tests = [
+            "conv1",
+            "conv3_x",
+            "conv4_1",
         ]
     elif args.config == "full":
         width, height = 32, 16
@@ -191,8 +206,30 @@ def dispatch(args, extra_args=None):
             #"handcrafted/resnet_pond",
             #"handcrafted/pond_and_mem"
         ]
-        #glb_tests = [
-        #]
+        resnet_tests = [
+            "conv1",
+            "conv2_x",
+            "conv3_1",
+            "conv3_x",
+            "conv4_1",
+            "conv4_x",
+            "conv5_1",
+            "conv5_x",
+        ]
+    elif args.config == "resnet":
+        width, height = 32, 16
+        tests = []
+        glb_tests = []
+        resnet_tests = [
+            "conv1",
+            "conv2_x",
+            "conv3_1",
+            "conv3_x",
+            "conv4_1",
+            "conv4_x",
+            "conv5_1",
+            "conv5_x",
+        ]
 
     else:
         raise NotImplementedError(f"Unknown test config: {config}")
@@ -201,12 +238,42 @@ def dispatch(args, extra_args=None):
     info = []
     t = gen_garnet(width, height)
     info.append(["garnet", t])
+    
+    halide_gen_args = {}
+    halide_gen_args["apps/gaussian"] = "mywidth=184 myunroll=8 schedule=3"
+
     for test in tests:
+        if test in halide_gen_args:
+            os.environ["HALIDE_GEN_ARGS"] = halide_gen_args[test]
+        else:
+            os.environ["HALIDE_GEN_ARGS"] = ""
         t0, t1, t2 = run_test(test, width, height)
         info.append([test, t0 + t1 + t2, t0, t1, t2])
         print(tabulate(info, headers=["step", "total", "compile", "map", "test"]))
     for test in glb_tests:
+        if test in halide_gen_args:
+            os.environ["HALIDE_GEN_ARGS"] = halide_gen_args[test]
+        else:
+            os.environ["HALIDE_GEN_ARGS"] = ""
         t0, t1, t2 = run_glb(test, width, height)
+    for test in resnet_tests:
+        if test == "conv1":
+            os.environ["HALIDE_GEN_ARGS"] = "in_img=32 pad=3 ksize=7 stride=2 n_ic=3 n_oc=64 k_ic=3 k_oc=16" 
+        elif test == "conv2_x":
+            os.environ["HALIDE_GEN_ARGS"] = "in_img=56 pad=1 ksize=3 stride=1 n_ic=16 n_oc=16 k_ic=8 k_oc=8" 
+        elif test == "conv3_1":
+            os.environ["HALIDE_GEN_ARGS"] = "in_img=56 pad=1 ksize=3 stride=2 n_ic=16 n_oc=16 k_ic=8 k_oc=8" 
+        elif test == "conv3_x":
+            os.environ["HALIDE_GEN_ARGS"] = "in_img=28 pad=1 ksize=3 stride=1 n_ic=16 n_oc=16 k_ic=8 k_oc=8" 
+        elif test == "conv4_1":
+            os.environ["HALIDE_GEN_ARGS"] = "in_img=28 pad=1 ksize=3 stride=2 n_ic=16 n_oc=16 k_ic=8 k_oc=8" 
+        elif test == "conv4_x":
+            os.environ["HALIDE_GEN_ARGS"] = "in_img=14 pad=1 ksize=3 stride=1 n_ic=16 n_oc=16 k_ic=8 k_oc=8" 
+        elif test == "conv5_1":
+            os.environ["HALIDE_GEN_ARGS"] = "in_img=14 pad=1 ksize=3 stride=2 n_ic=16 n_oc=16 k_ic=8 k_oc=8" 
+        elif test == "conv5_x":
+            os.environ["HALIDE_GEN_ARGS"] = "in_img=7 pad=1 ksize=3 stride=1 n_ic=16 n_oc=16 k_ic=8 k_oc=8" 
+        t0, t1, t2 = run_glb("apps/resnet_output_stationary", width, height, test)
         info.append([test + "_glb", t0 + t1 + t2, t0, t1, t2])
         print(tabulate(info, headers=["step", "total", "compile", "map", "test"]))
 
