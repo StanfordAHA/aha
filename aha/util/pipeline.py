658ca880a7c4041c379d25448388ddf3ada19d27
jack-melchert <56329001+jack-melchert@users.noreply.github.com>
2022-04-26 08:58:53 -0700

    Update pipeline.py

--- aha/util/pipeline.py
+++ aha/util/pipeline.py
@@ -15,6 +15,11 @@ def add_subparser(subparser):
 def dispatch(args, extra_args=None):
     args.app = Path(args.app)
     env = copy.deepcopy(os.environ)
+    env["COREIR_DIR"] = str(args.aha_dir / "coreir")
+    env["COREIR_PATH"] = str(args.aha_dir / "coreir")
+    env["LAKE_PATH"] = str(args.aha_dir / "lake")
+    env["CLOCKWORK_PATH"] = str(args.aha_dir / "clockwork")
+    
     if args.base is None:
         app_dir = Path(
             f"{args.aha_dir}/Halide-to-Hardware/apps/hardware_benchmarks/{args.app}"
