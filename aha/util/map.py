6a16792571f4327e1dcfc5ed52eddbd661dea965
Po-Han Chen <pohan@stanford.edu>
2022-04-28 21:32:11 -0700

    fix missing stderr problem, change glb report target

--- aha/util/map.py
+++ aha/util/map.py
@@ -15,14 +15,16 @@ def add_subparser(subparser):
 
 def subprocess_call_log(cmd, cwd, log, log_file_path):
     if log:
-        proc = subprocess.Popen(
-            cmd,
-            cwd=cwd,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-        subprocess.check_call(["tee", log_file_path, "-a"], stdin=proc.stdout)
-        proc.wait()
+        print("[log] Command  : {}".format(cmd))
+        print("[log] Log Path : {}".format(log_file_path), end="  ...", flush=True)
+        with open(log_file_path, "a") as flog:
+            subprocess.check_call(
+                cmd,
+                cwd=cwd,
+                stdout=flog,
+                stderr=flog
+            )
+        print("done")
     else:
         subprocess.check_call(
             cmd,
