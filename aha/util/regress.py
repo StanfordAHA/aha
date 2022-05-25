917fd2e6456b5d4cd54d82404e62f3452f501f8b
root <melchert@stanford.edu>
2022-05-25 13:32:12 -0700

    Resnet with hard flush should work now

--- aha/util/regress.py
+++ aha/util/regress.py
@@ -51,8 +51,6 @@ def gen_garnet(width, height):
         str(height),
         "--verilog",
         "--use_sim_sram",
-        "--pipeline_config_interval",
-        str(16)
     ])
     return time.time() - start
 
@@ -78,17 +76,10 @@ def run_glb(testname, width, height, test=''):
     my_env = {}
     my_env = {'DISABLE_GP': '1'}
 
-    # Hack needed to get around scheduling limitation, will remove later
-    if "resnet" in testname:
-        buildkite_call(
-            ["aha", "pipeline", testname, "--width", str(width), "--height", str(height), "--pipeline_config_interval", str(16), "--no-input-broadcast-pipelining"],
-            env=my_env
-        )
-    else:        
-        buildkite_call(
-            ["aha", "pipeline", testname, "--width", str(width), "--height", str(height), "--pipeline_config_interval", str(16)],
-            env=my_env
-        )
+    buildkite_call(
+        ["aha", "pipeline", testname, "--width", str(width), "--height", str(height)],
+        env=my_env
+    )
     time_map = time.time() - start
 
     print(f"--- {test} - glb testing")
