d941431b50e4b86c034b6e781048a0036579ff0d
steveri <steveri@steveri.com>
2023-04-11 14:14:39 -0700

    back to monke

--- aha/util/regress.py
+++ aha/util/regress.py
@@ -100,9 +100,8 @@ def test_sparse_app(testname, width, height, test=""):
     return 0, time_map, time_test
 
 def test_dense_app(test, width, height, layer=None, env_parameters=""):
-    testname = layer if layer is not None else test
-    print(f"--- {testname}")
-    print(f"--- {testname} - compiling and mapping")
+    print(f"--- {test}")
+    print(f"--- {test} - compiling")
     app_path = "/aha/Halide-to-Hardware/apps/hardware_benchmarks/" + test
     print(app_path)
 
@@ -117,10 +116,10 @@ def test_dense_app(test, width, height, layer=None, env_parameters=""):
         pass
 
     start = time.time()
-    buildkite_call(["aha", "map", test, "--chain", "--env-parameters", env_parameters] + layer_array)
+    buildkite_call(["aha", "map", test, "--env-parameters", env_parameters] + layer_array)
     time_compile = time.time() - start
 
-    print(f"--- {testname} - pnr and pipelining")
+    print(f"--- {test} - mapping")
     start = time.time()
 
     buildkite_call(
@@ -130,12 +129,12 @@ def test_dense_app(test, width, height, layer=None, env_parameters=""):
             test,
             "--width", str(width),
             "--height", str(height),
-            "--env-parameters", env_parameters,
+            "--env-parameters", env_parameters
         ] + layer_array
     )
     time_map = time.time() - start
 
-    print(f"--- {testname} - glb testing")
+    print(f"--- {test} - glb testing")
     start = time.time()
     buildkite_call(["aha", "test", test])
     time_test = time.time() - start
@@ -299,7 +298,9 @@ def dispatch(args, extra_args=None):
 #         t0, t1, t2 = test_dense_app(test, width, height, env_parameters=str(args.env_parameters))
 #         info.append([test + "_glb", t0 + t1 + t2, t0, t1, t2])
 
-    resnet_tests = [        "conv3_x",    ]
+    resnet_tests = [
+        "conv3_x",
+    ]
     for test in resnet_tests:
         t0, t1, t2 = test_dense_app("apps/resnet_output_stationary", width, height, layer=test, env_parameters=str(args.env_parameters))
         info.append([test + "_glb", t0 + t1 + t2, t0, t1, t2])
