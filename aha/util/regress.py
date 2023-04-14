769769fb8b93885dcde7b77e7301b80c8e0da85c
steveri <steveri@steveri.com>
2023-04-14 09:21:21 -0700

    foo

--- aha/util/regress.py
+++ aha/util/regress.py
@@ -117,7 +117,7 @@ def test_dense_app(test, width, height, layer=None, env_parameters=""):
         pass
 
     start = time.time()
-    buildkite_call(["aha", "map", test, "--chain", "--env-parameters", env_parameters] + layer_array)
+    buildkite_call(["aha", "map", test,            "--env-parameters", env_parameters] + layer_array)
     time_compile = time.time() - start
 
     print(f"--- {testname} - pnr and pipelining")
@@ -291,14 +291,15 @@ def dispatch(args, extra_args=None):
     t = gen_garnet(width, height)
     info.append(["garnet", t])
 
-    for test in sparse_tests:
-        t0, t1, t2 = test_sparse_app(test, width, height)
-        info.append([test + "_glb", t0 + t1 + t2, t0, t1, t2])
+#     for test in sparse_tests:
+#         t0, t1, t2 = test_sparse_app(test, width, height)
+#         info.append([test + "_glb", t0 + t1 + t2, t0, t1, t2])
+# 
+#     for test in glb_tests:
+#         t0, t1, t2 = test_dense_app(test, width, height, env_parameters=str(args.env_parameters))
+#         info.append([test + "_glb", t0 + t1 + t2, t0, t1, t2])
 
-    for test in glb_tests:
-        t0, t1, t2 = test_dense_app(test, width, height, env_parameters=str(args.env_parameters))
-        info.append([test + "_glb", t0 + t1 + t2, t0, t1, t2])
-
+    resnet_tests = [        "conv3_x",    ]
     for test in resnet_tests:
         t0, t1, t2 = test_dense_app("apps/resnet_output_stationary", width, height, layer=test, env_parameters=str(args.env_parameters))
         info.append([test + "_glb", t0 + t1 + t2, t0, t1, t2])
