e6b3572e9baade54c468ab840fe530dc38a35bf6
steveri <steveri@steveri.com>
2023-08-04 08:58:42 -0700

    clean up pipeline upload script

--- .buildkite/bin/upload-pipeline.sh
+++ .buildkite/bin/upload-pipeline.sh
@@ -6,14 +6,14 @@ set +u # nounset? not on my watch!
 
 echo "--- upload-pipeline BEGIN"
 
+# Remote locations for pipeline, checkout scripts
+# (Unique BUILD_NUMBER query at end of url prevents caching)
+p_remote=pipeline.yml?$BUILDKITE_BUILD_NUMBER
+c_remote=bin/custom-checkout.sh?$BUILDKITE_BUILD_NUMBER
 
-set -x
-
-# Unique BUILD_NUMBER query at end of url prevents caching
-p=pipeline.yml?$BUILDKITE_BUILD_NUMBER
-ptmp=/tmp/aha-flow-$BUILDKITE_BUILD_NUMBER-pipeline.xml
-c=bin/custom-checkout.sh?$BUILDKITE_BUILD_NUMBER
-ctmp=/tmp/aha-flow-$BUILDKITE_BUILD_NUMBER-custom-checkout.sh
+# Where to put scripts (locally) when we find them
+p_local=/tmp/aha-flow-$BUILDKITE_BUILD_NUMBER-pipeline.xml
+c_local=/tmp/aha-flow-$BUILDKITE_BUILD_NUMBER-custom-checkout.sh
 
 # Various places we might find pipeline.xml
 u=https://raw.githubusercontent.com/StanfordAHA/aha
@@ -21,34 +21,39 @@ amaster=$u/master/.buildkite
 acommit=$u/$BUILDKITE_COMMIT/.buildkite
 adev=$u/no-heroku/.buildkite
 
-# Establish baselines. Okay (for now) if checkout.sh does not exist
-curl -s --fail $amaster/$p > $ptmp; curl -s $amaster/$c > $ctmp
+# Establish baselines
+curl -s --fail $amaster/$p_remote > $p_local
+curl -s        $amaster/$c_remote > $c_local  # Okay (for now) if checkout.sh does not exist
 echo Established default pipeline foo
 
-ls /tmp/*/pre-command || echo nop
-cat -n /tmp/*/pre-command || echo nop
+# ls /tmp/*/pre-command || echo nop
+# cat -n /tmp/*/pre-command || echo nop
 
 for i in 1; do set -x
     # Test this path by doing "git pull master" from a submodule
     [ "$FLOW_HEAD_SHA" ] && break    # (heroku uses pipeline from master)
     echo Not heroku, stay w default
 
-    # curl -s $acommit/$p > $ptmp  && break || echo Not triggered by aha repo
+    # curl -s $acommit/$p_remote > $p_local  && break || echo Not triggered by aha repo
     # If acommit exists, trigger came from aha repo. Okay (for now) if checkout.sh does not exist
     # Test this path by doing "git push" from aha repo
-    if curl -sf $acommit/$p > $ptmp; then curl -s  $acommit/$c > $ctmp; break; fi
+    if curl -sf $acommit/$p_remote > $p_local; then
+       curl -s  $acommit/$c_remote > $c_local; break
+    fi
     echo Not triggered by aha repo
 
-    # curl -s $adev/$p > $ptmp  && break || echo Cannot find dev pipeline
+    # curl -s $adev/$p_remote > $p_local  && break || echo Cannot find dev pipeline
     # Not heroku; not aha; this is the new stuff
     # Triggered by non-aha repo (i.e. just garnet for now)
     # Use dev pipe if exists, else stick with master
     # Test this path (for now) by doing a "git push" from garnet repo
-    if curl -sf $adev/$p > $ptmp; then curl -sf $adev/$c > $ctmp; break; fi
+    if curl -sf $adev/$p_remote > $p_local; then
+       curl -sf $adev/$c_remote > $c_local; break
+    fi
     echo Cannot find dev pipeline, will use master instead
 done
 
-buildkite-agent pipeline upload $ptmp
+buildkite-agent pipeline upload $p_local
 
 echo "--- RESTORE SHELLOPTS"; eval "$RESTORE_SHELLOPTS"
 echo "--- upload-pipeline END"
