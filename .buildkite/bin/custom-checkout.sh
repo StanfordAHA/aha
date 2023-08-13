207e205b73c1eb838e2a55c7dc021cea3cf54e82
steveri <steveri@steveri.com>
2023-08-13 10:08:09 -0700

    clean up custom-checkout.sh script

--- .buildkite/bin/custom-checkout.sh
+++ .buildkite/bin/custom-checkout.sh
@@ -11,23 +11,14 @@ set +u    # nounset? not on my watch!
 set +x    # debug OFF
 PS4="_"   # Prevents "+++" prefix during 3-deep set -x execution
 
+# Checkout
 echo "+++ custom-checkout.sh BEGIN"
-
-# BUILDKITE_BUILD_CHECKOUT_PATH=/var/lib/buildkite-agent/builds/r7cad-docker-1/stanford-aha/aha-flow
 echo I am `whoami`     # Watch out if this ever says "I am root"
 echo I am in dir `pwd` # Watch out if this ever we are in root dir (/)
 
-set +x
-echo "+++ Continue custom-checkout.sh"
-
-# # IF this works it enables all kinds of optimiztions
-# # Okay. Like what for example?
-# echo FLOW_REPO=$FLOW_REPO || echo nop
-# echo FLOW_HEAD_SHA=$FLOW_HEAD_SHA || echo nop
-# echo '-------------'
- 
 echo "--- PREP AHA REPO and all its submodules"; set -x
 pwd
+# E.g. CHECKOUT_PATH=/var/lib/buildkite-agent/builds/r7cad-docker-1/stanford-aha/aha-flow
 cd $BUILDKITE_BUILD_CHECKOUT_PATH # Actually I think we're already there but whatevs
 
 git submodule update --checkout # This is probably unnecessary but whatevs
@@ -37,14 +28,27 @@ git clean -ffxdq
 set +x
 
 # Would this work for heroku maybe? Surely this would work for heroku.
-# if [[ $$FLOW_REPO ]]; then
+
+# Heroku always sets message to "PR from <repo>" with BUILDKITE_COMMIT="HEAD"
+# so as to checkout aha master. Heroku sends desired submod commit <repo>
+# hash as env var FLOW_HEAD_SHA.  In this new regime, we set BUILDKITE_COMMIT
+# as the desired submod commit, and auto-discover the repo that goes with the commit.
+
 if expr "$$BUILDKITE_MESSAGE" : "PR from "; then
     BUILDKITE_COMMIT=FLOW_HEAD_SHA
 fi
 
-echo "--- Check out appropriate AHA branch"
+AHA_DEFAULT_BRANCH=no-heroku
+echo "--- Check out appropriate AHA branch: $BUILDKITE_COMMIT, $AHA_DEFAULT_BRANCH, or master"
 unset PR_FROM_SUBMOD
-# PR_FROM_SUBMOD means build was triggered by foreign (non-aha) repo, i.e. one of the submods
+
+# PR_FROM_SUBMOD means build was triggered by foreign (non-aha) repo,
+# i.e. one of the submods. The submod sends its commit hash BUT DOES
+# NOT TELL US WHAT REPO IT IS. :(
+
+# We detect this by attempting to checkout BUILDKITE_COMMIT from aha repo.
+# Success means this is an aha-triggered build. Failure means we need to
+# find what repo actually did trigger the commit.
 
 echo git fetch -v --prune -- origin $BUILDKITE_COMMIT
 if   git fetch -v --prune -- origin $BUILDKITE_COMMIT; then
@@ -56,12 +60,20 @@ else
     echo 'This must be a pull request from one of the submods'
     PR_FROM_SUBMOD=true
 
-    AHA_DEFAULT_BRANCH=master
+    # FIXME don't need if-then-else below fter dev merges to master.
+    
+    # Use dev branch as default until it gets merged and deleted.
     AHA_DEFAULT_BRANCH=no-heroku
     echo "Meanwhile, will use default branch '$AHA_DEFAULT_BRANCH' for aha repo"
-    git fetch -v --prune -- origin $AHA_DEFAULT_BRANCH
-    git checkout -f $AHA_DEFAULT_BRANCH
-    echo "Checked out aha default branch '$AHA_DEFAULT_BRANCH'"
+    if git fetch -v --prune -- origin $AHA_DEFAULT_BRANCH; then
+        echo "Fetching aha branch '$AHA_DEFAULT_BRANCH'"
+        git checkout -f $AHA_DEFAULT_BRANCH
+    else
+        echo "Aha branch '$AHA_DEFAULT_BRANCH' does not exist"
+        echo "Fetching aha master branch"
+        git fetch -v --prune -- origin master
+        git checkout -f master
+    fi
 fi
 
 set -x
@@ -71,6 +83,10 @@ git submodule update --init --recursive --force
 echo '--- git submodule foreach --recursive "git reset --hard"'
 git submodule foreach --recursive "git reset --hard"
 set +x
+
+# To find out what repo triggered the commit, we iterate through
+# all the submodules and find which one can successfully checkout
+# the desired BUILKITE_COMMIT.
 
 if [ "$PR_FROM_SUBMOD" ]; then
     echo "--- Handle PR"
