1d7e5fca5fbc9a18d5e019133cc957d861bb26ed
steveri <steveri@steveri.com>
2023-08-23 12:21:56 -0700

    add fault to the mix; temporary fix for a bigger problem

--- .buildkite/bin/custom-checkout.sh
+++ .buildkite/bin/custom-checkout.sh
@@ -160,7 +160,7 @@ if [ "$PR_FROM_SUBMOD" ]; then
     echo "--- Handle PR"
     echo "+++ Looking for submod commit $BUILDKITE_COMMIT"
     unset FOUND_SUBMOD
-    for submod in garnet Halide-to-Hardware lassen gemstone canal lake hwtypes; do
+    for submod in garnet Halide-to-Hardware lassen gemstone canal lake hwtypes fault; do
         echo "--- - " Looking in submod $submod
         # --- THIS IS WHERE THE CHECKOUT HAPPENS ---
         (set -x; cd $submod; git fetch origin && git checkout $BUILDKITE_COMMIT) && FOUND_SUBMOD=true || echo "NOT " $submod
