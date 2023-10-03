6b863b5c1c3be0800168f38f1937740aed8c28f1
steveri <steveri@steveri.com>
2023-10-03 14:21:15 -0700

    Container startup script must have means to execute "docker run" command

--- aha/bin/restore-halide-distrib.sh
+++ aha/bin/restore-halide-distrib.sh
@@ -31,3 +31,7 @@ else
 fi
 ls -lh /aha/Halide-to-Hardware/distrib/{bin,lib}/*
 echo ""
+
+# When this script runs as a Dockerfile ENTRYPOINT, this next line
+# allows execution of further commands passed via e.g. "docker run"
+exec "$@"
