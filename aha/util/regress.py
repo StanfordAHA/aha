23276117a297594d5330c48d7b8d96fc04e55256
steveri <steveri@steveri.com>
2023-12-07 16:19:01 -0800

    hardwired --daemon,auto

--- aha/util/regress.py
+++ aha/util/regress.py
@@ -148,7 +148,7 @@ def test_dense_app(test, width, height, layer=None, env_parameters=""):
             test,
             "--width", str(width),
             "--height", str(height),
-            "--daemon auto",
+            "--daemon", "auto",
             "--env-parameters", env_parameters,
         ] + layer_array
     )
