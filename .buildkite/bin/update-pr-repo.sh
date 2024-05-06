b48ae176f7b1608a5b06930adf66d1cca1d50e06
steveri <steveri@steveri.com>
2024-05-06 09:58:30 -0700

    fetched new update-pr-repo.sh

--- .buildkite/bin/update-pr-repo.sh
+++ .buildkite/bin/update-pr-repo.sh
@@ -13,6 +13,9 @@ cd $BUILDKITE_BUILD_CHECKOUT_PATH    # Just in case, I dunno, whatevs.
 
 # Also need to rediscover pull number BUILDKITE_PULL_REQUEST
 
+# This script is sourced only from pipeline.yml
+# and only when AHA_SUBMOD_FLOW_COMMIT exists i.e.:
+# [ "$$AHA_SUBMOD_FLOW_COMMIT" ] && source $$bin/update-pr-repo.sh
 
 echo "- Reset BUILDKITE_COMMIT according to env var set by aha-submod-flow steps :("
 echo "- https://buildkite.com/stanford-aha/aha-submod-flow/settings/steps"
