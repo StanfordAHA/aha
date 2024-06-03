4c0719597e3000171fac488bcff8a36beeb410b1
steveri <steveri@steveri.com>
2024-06-03 10:40:05 -0700

    implement retry strategy (#1912)
    * implement retry strategy
    * better fix, does not have to restart from beginning every time maybe

--- aha/util/garnet.py
+++ aha/util/garnet.py
@@ -10,6 +10,21 @@ def add_subparser(subparser):
 
 
 def dispatch(args, extra_args=None):
-    subprocess.check_call(
-        [sys.executable, "garnet.py"] + extra_args, cwd=args.aha_dir / "garnet",
-    )
+    for retry in [1,2,3]:  # In case of SIGSEGV, retry up to three times
+        try:
+            subprocess.check_call(
+                [sys.executable, "garnet.py"] + extra_args, cwd=args.aha_dir / "garnet",
+            )
+            break
+        except subprocess.CalledProcessError as e:
+            if 'SIGSEGV' in str(e):
+                print(f'\n\n{e}\n')  # Print the error msg
+                print(f'*** ERROR subprocess died {retry} time(s) with SIGSEGV')
+                print('*** Will retry three times, then give up.\n\n')
+
+                # if retry == 3: raise
+                # - No! Don't raise the error! Higher-level aha.py has similar
+                # - three-retry catchall, resulting in up to nine retries ! (Right?)
+                # - Do this instead:
+                if retry == 3:
+                    assert False, 'ERROR: Three time loser'
