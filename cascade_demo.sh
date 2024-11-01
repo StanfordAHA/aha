81b0351a0022f8593ecba3890e912bc24e7991e9
root <melchert@stanford.edu>
2024-10-31 19:39:32 -0700

    Update for cascade and apex demos

--- cascade_demo.sh
+++ cascade_demo.sh
@@ -4,5 +4,5 @@ pip install Pillow
 
 POST_PNR_ITR=$1 PYTHONPATH=/aha/garnet/ python /aha/garnet/tests/test_memory_core/build_tb.py --ic_fork --sam_graph /aha/sam/compiler/sam-outputs/onyx-dot/mat_elemadd.gv --seed 0 --dump_bitstream --add_pond --combined --pipeline_scanner --base_dir /aha/garnet/SPARSE_TESTS/ --just_glb --dump_glb --fiber_access --width 12 --height 4
 
-python archipelago/archipelago/sta.py -a garnet/SIM_DIR/ -v
+python archipelago/archipelago/sta.py -a garnet/SIM_DIR/ -v --sparse
 
