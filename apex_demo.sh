3dc737c2a1f3c2fb93467b427258ed2bcb9bb59f
root <melchert@stanford.edu>
2024-11-01 10:52:35 -0700

    Added pip install requirements to apex demo

--- apex_demo.sh
+++ apex_demo.sh
@@ -1,6 +1,7 @@
 if [ "$1" = "mine" ]
 then
 cd APEX
+pip install -r requirements.txt
 python dse_graph_analysis.py -f ../Halide-to-Hardware/apps/hardware_benchmarks/apps/gaussian/bin/gaussian_compute.json 20 -f ../Halide-to-Hardware/apps/hardware_benchmarks/apps/harris/bin/harris_compute.json 20 --subgraph-mining-only
 cd ..
 fi
