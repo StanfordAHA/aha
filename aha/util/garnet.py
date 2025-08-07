6d9efd29e7c071aa5c86b25a6adf6699e2d470eb
steveri <steveri@steveri.com>
2025-08-07 15:34:15 -0700

    fixed retry mechanism a bit

--- aha/util/garnet.py
+++ aha/util/garnet.py
@@ -9,13 +9,13 @@ def add_subparser(subparser):
     parser.set_defaults(dispatch=dispatch)
 
 
-def dispatch(args, extra_args=None):
+# FIXME/TODO this function is same in pnr.py, granet.py, regress.py...
+def retry(r_func, *args, **kwargs):
     for retry in [1, 2, 3]:  # In case of SIG error, retry up to three times
+        print(f"--- TRY attempt #{retry} {args=}", flush=True)
         try:
-            subprocess.check_call(
-                [sys.executable, "garnet.py"] + extra_args, cwd=args.aha_dir / "garnet",
-            )
-            break
+            r_func(*args, **kwargs); break
+
         except subprocess.CalledProcessError as e:
             retry_sigs = [ 'SIGSEGV','SIGBUS','SIGABRT']; found_retry_sig = False
             for rs in retry_sigs:
@@ -24,7 +24,7 @@ def dispatch(args, extra_args=None):
             if found_retry_sig:
                 print(f'\n\n{e}\n')  # Print the error msg
                 print(f'*** ERROR subprocess died {retry} time(s) with one of {retry_sigs}')
-                print('*** Will retry three times, then give up.\n\n')
+                print('*** Will retry twice more, then give up.\n\n', flush=True)
 
                 # if retry == 3: raise
                 # - No! Don't raise the error! Higher-level aha.py has similar
@@ -34,3 +34,9 @@ def dispatch(args, extra_args=None):
                     assert False, 'ERROR: Three time loser'
             else:
                 raise
+
+def dispatch(args, extra_args=None):
+    retry(
+        subprocess.check_call,
+        [sys.executable, "garnet.py"] + extra_args, cwd=args.aha_dir / "garnet"
+    )
