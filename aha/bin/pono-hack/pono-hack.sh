0a0edaad86433181a3cd5a7cb9da6f08f6526ad0
steveri <steveri@steveri.com>
2025-08-12 06:22:16 -0700

    cleanup

--- aha/bin/pono-hack/pono-hack.sh
+++ aha/bin/pono-hack/pono-hack.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
+set -x
 
 # Where is cmake? E.g. cmdir=/usr/local/lib/python3.8/dist-packages/cmake
 python3 -c 'import cmake; print(cmake.__file__)'
 cmdir=`python3 -c 'import cmake; print(cmake.__file__)' | sed 's/.__init__.py//'`
+cversion=`cmake --version | awk '{print $NF; exit}'`  # Should be "3.28.1" maybe
 
 # Dockerfile copies this script to /aha/pono/contrib/pono-hack/pono-hack.sh
 if [ "$1" == '--install' ]; then
-
     # Replace "official" cmake with this script
-    echo FOOOOOOOOOOOOOOOOOOOO
-
-    set -x
 
     # Move old cmake to cmake_orig
     if test -e $cmdir/data/bin/cmake_orig; then
@@ -31,52 +29,49 @@ if [ "$1" == '--install' ]; then
         cp -p $cmdir/__init__.py $cmdir/__init__.py.bku
     fi
     sed -i "/_program.*'cmake'/s/cmake/cmake_orig/" $cmdir/__init__.py
+
+    # Verify the change:
+    #   <  _program('cmake',      *sys.argv[1:])
+    #   >  _program('cmake_orig', *sys.argv[1:])
     diff $cmdir/__init__.py.bku $cmdir/__init__.py || echo okay
+
+elif [ "$1" == '--uninstall' ]; then
+    # Undo the hack and put cmake back the way it was I hope
+
+    echo "pono-hack: pip uninstall hacked cmake"
+    old_cmake=/aha/lib/python3.8/site-packages/cmake
+    test -e ${cmdir} && mv ${cmdir} ${cmdir}-orig-deleteme
+
+    echo "pono-hack: pip (re)install cmake==$cversion"
+    pip install cmake==$cversion
 
 else
     # Pass args to real cmake, now renamed cmake_orig
 
-    python3 -c 'import cmake; print(cmake.__file__)'
-    d=`python3 -c 'import cmake; print(cmake.__file__)' | sed 's/.__init__.py//'`
+    echo "$$ WARNING Using hacked cmake, see /aha/aha/bin/pono-hack"
 
-    echo '$$ fooooooo and hello i am fake cmake hahahahaha'
+    echo "xx$@xx" | grep guess-download-Production && echo "$$ pono-hack: subverting config.guess download..."
+    echo "xx$@xx" | grep sub-download-Production   && echo "$$ pono-hack: subverting config.sub download..."
 
-    echo "xx$@xx" | grep guess-download-Production && echo "$$ barrrrrrr guess what"
-    echo "xx$@xx" | grep sub-download-Production && echo "$$ barrrrrrr what sub"
-
-    echo "$$ I SEE ARGS"
-    for a in "$@"; do echo "$$  '$a'"; done
-    echo ''
+    # Looking for this arg pattern:
+    # '-P' '/aha/pono/deps/smt-switch/deps/cvc5/build/deps/src/ANTLR3-EP-config.guess-stamp/\
+    #            ANTLR3-EP-config.guess-download-Production.cmake'
+    # echo "$$ I SEE ARGS"; for a in "$@"; do echo "$$  '$a'"; done; echo ''
 
     # Dockerfile should have already copied config.{sub,guess} to /aha/pono/contrib/pono-hack/
     src=/aha/pono/contrib/pono-hack
     dst=/aha/pono/deps/smt-switch/deps/cvc5/build/deps/src
     if echo "xx$@xx" | grep guess-download-Production; then
-        echo "$$ barrr copy 'config.guess' and exit"
+        echo "$$ pono-hack: copy 'config.guess' and exit"
         cp $src/config.guess $dst
         ls -l $src/config.guess $dst/config.guess
     elif echo "xx$@xx" | grep sub-download-Production; then
-        echo "$$ barrr copy 'config.sub' and exit"
+        echo "$$ pono-hack: copy 'config.sub' and exit"
         cp $src/config.sub $dst
         ls -l $src/config.sub $dst/config.sub
     else
-        echo 'fooo passing args through to real cmake'
+        echo "$$ pono-hack: passing args through to real cmake"
         exec $cmdir/data/bin/cmake_orig "$@"
     fi
 fi
 exit 0  # Success!
-
-#   <  _program_exit('cmake',      *sys.argv[1:])
-#   >  _program_exit('cmake_orig', *sys.argv[1:])
-
-
-# ls -l /aha/pono/deps/smt-switch/deps/cvc5/build/deps/src/ANTLR3-EP-config.sub-stamp/ANTLR3-EP-config.sub-download-Production.cmake
-# /aha/pono/deps/smt-switch/deps/cvc5/build/deps/src/ANTLR3-EP-config.sub-stamp/ANTLR3-EP-config.sub-download*.log
-# ls -l /aha/pono/deps/smt-switch/deps/cvc5/build/deps/src/ANTLR3-EP-config.sub-stamp/ANTLR3-EP-config.sub-download*.log
-# 
-# 
-# 3396070 I SEE ARGS
-# 3396070  '-P'
-# 3396070  '/aha/pono/deps/smt-switch/deps/cvc5/build/deps/src/ANTLR3-EP-config.guess-stamp/ANTLR3-EP-config.guess-download-Production.cmake'
-
-# >>> import cmake; print(cmake.__file__)
