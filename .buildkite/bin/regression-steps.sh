844f71fb3e7a258041d3ae5c5da73ad2fb37e648
steveri <steveri@steveri.com>
2025-09-10 13:07:39 -0700

    try several threes at once, see what breaks...

--- .buildkite/bin/regression-steps.sh
+++ .buildkite/bin/regression-steps.sh
@@ -142,7 +142,7 @@ CONCURRENCY="
 # Each new step uploads only after previous step has started running.
 # E.g. NSTEPS="0 1 2 3 4 5 6 7 8 9"
 # NSTEPS="0 1 3 4 5"  # Trying to reproduce intermittent 'missing design.place' error e.g. Regress 3 on build 12067
-NSTEPS="3"  # Trying to reproduce intermittent 'missing design.place' error e.g. Regress 3 on build 12067
+NSTEPS="3 3 3 3 3 3"  # Trying to reproduce intermittent 'missing design.place' error e.g. Regress 3 on build 12067
 for i in $NSTEPS; do
     [ "$i" == 0 ] && label="Fast" || label="Regress $i"
     setstate launch-state READY
