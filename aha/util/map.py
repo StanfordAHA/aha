c6dba19e9bbe5f3829aca535518fd29757adaf9c
Michael Oduoza <mcoduoza@gmail.com>
2025-10-13 21:41:26 -0700

    Remove breakpoints previously added for debugging

--- aha/util/map.py
+++ aha/util/map.py
@@ -250,8 +250,6 @@ def dispatch(args, extra_args=None):
         print(f"\033[93mINFO: Skipping CGRA mapping for {app_name} as per settings in regress.py. Assuming mapping collateral has previously been generated...\033[0m")
     else:
         print(f"\033[92mMapping {app_name} to CGRA...\033[0m")
-
-        breakpoint()
         # Copy path_balance.json if using POND_PATH_BALANCING
         if 'POND_PATH_BALANCING' in env and env['POND_PATH_BALANCING'] == '1':
             layer_path_balance_folder = f"{app_dir}/path_balancing_configs"
@@ -259,7 +257,6 @@ def dispatch(args, extra_args=None):
             assert model is not None and layer is not None, f"ERROR: mu_test must be specified to use POND_PATH_BALANCING"
 
             layer_path_balance_json = os.path.join(layer_path_balance_folder, f"{model}-{layer}_path_balancing.json")
-            breakpoint()
             assert os.path.exists(layer_path_balance_json), f"ERROR: {model}_{layer}_path_balancing.json not found in {layer_path_balance_folder}."
             os.system(f"cp {layer_path_balance_json} {app_dir}/bin/path_balancing.json")
 
