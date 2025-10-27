3545f0a409e9b36fe00e8d7209398a2632f4593a
steveri <steveri@steveri.com>
2025-10-27 15:47:19 -0700

    no skip regressions on garnet push

--- .buildkite/bin/regress-metahooks.sh
+++ .buildkite/bin/regress-metahooks.sh
@@ -64,7 +64,9 @@ if [ "$1" == '--pre-command' ]; then
 
     echo "--- Pass DO_PR info to docker"
     echo "Info gets passed to docker by mounting temp dir as /buildkite omg omg"
-    if [ "$REQUEST_TYPE" == "SUBMOD_PR" ]; then
+
+    # if [ "$REQUEST_TYPE" == "SUBMOD_PR" ]; then
+    if ! [ "BUILDKITE_PULL_REQUEST" == "false" ]; then
         echo ".. SET DO_PR"; mkdir -p temp; touch temp/DO_PR
         if [ "$PR_REPO_TAIL" == "garnet" ]; then
             # Delete .TEST as a sign to skip tests
