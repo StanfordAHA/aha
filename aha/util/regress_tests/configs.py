f6d7e65eb5192df207acb965a4abfbd026d00421
steveri <steveri@steveri.com>
2025-10-31 16:40:09 -0700

    sync configs/tests

--- aha/util/regress_tests/configs.py
+++ aha/util/regress_tests/configs.py
@@ -180,6 +180,85 @@ class Configs:
             'resnet18-submodule_17 -> zircon_residual_relu_fp_post_conv5_x_kernel2_RV_E64_MB',
             'resnet18-submodule_17 -> zircon_residual_relu_fp_post_conv5_x_kernel3_RV_E64_MB',]
     }
+    voyager_cgra_tests_fp = [
+        # Standalone quantize layers
+        "resnet18-quantize_default_1::zircon_quant_fp_post_conv2x_RV_E64_MB",
+        "resnet18-quantize_default_3::zircon_quant_fp_post_conv2x_RV_E64_MB",
+        "resnet18-quantize_default_7::zircon_quant_fp_post_conv3x_RV_E64_MB",
+        "resnet18-quantize_default_11::zircon_quant_fp_post_conv4x_RV_E64_MB",
+        "resnet18-quantize_default_15::zircon_quant_fp_post_conv5x_RV_E64_MB",
+    ]
+    external_mu_tests_fp2 = [
+        # Conv2_x
+        "resnet18-submodule_2 -> zircon_deq_q_relu_fp_post_conv2_x_RV_E64_MB",
+        "resnet18-submodule_3 -> zircon_deq_ResReLU_fp_post_conv2_x_RV_E64_MB",
+        "resnet18-submodule_4 -> zircon_deq_q_relu_fp_post_conv2_x_RV_E64_MB",
+        "resnet18-submodule_5 -> zircon_deq_ResReLU_quant_fp_post_conv2_x_RV_E64_MB",
+    ]
+    voyager_cgra_tests_fp2 = [
+        # Average pooling layer
+        "resnet18-adaptive_avg_pool2d_default_1::avgpool_layer_fp_RV_E64_MB",
+
+        # Fully connected layer (K-DIM HOST TILING)
+        "resnet18-linear::fully_connected_layer_fp_kernel0_RV_E64_MB",
+        "resnet18-linear::fully_connected_layer_fp_kernel1_RV_E64_MB",
+    ]
+    external_mu_tests_fp_all = [
+                # Conv1 (im2col-based, X-DIM HOST TILING)
+                "resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel0_RV_E64_MB",
+                "resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel1_RV_E64_MB",
+                "resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel2_RV_E64_MB",
+                "resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel3_RV_E64_MB",
+
+                # Conv2_x
+                "resnet18-submodule_2 -> zircon_deq_q_relu_fp_post_conv2_x_RV_E64_MB",
+                "resnet18-submodule_3 -> zircon_deq_ResReLU_fp_post_conv2_x_RV_E64_MB",
+                "resnet18-submodule_4 -> zircon_deq_q_relu_fp_post_conv2_x_RV_E64_MB",
+                "resnet18-submodule_5 -> zircon_deq_ResReLU_quant_fp_post_conv2_x_RV_E64_MB",
+
+                # Conv3_1 strided conv
+                "resnet18-submodule_6 -> zircon_deq_q_relu_fp_post_conv3_1_RV_E64_MB",
+
+                # Conv3_1 pointwise conv
+                "resnet18-submodule_7 -> zircon_dequant_fp_post_conv3_1_RV_E64_MB",
+
+                # Conv3_x
+                "resnet18-submodule_8 -> zircon_deq_ResReLU_fp_post_conv3_x_RV_E64_MB",
+                "resnet18-submodule_9 -> zircon_deq_q_relu_fp_post_conv3_x_RV_E64_MB",
+                "resnet18-submodule_10 -> zircon_deq_ResReLU_quant_fp_post_conv3_x_RV_E64_MB",
+
+                # Conv4_1 strided conv (TILED OUTER REDUCTION WORKAROUND)
+                "resnet18-submodule_11 -> zircon_nop_tiled_outer_reduction_workaround_post_conv4_1_RV_E64_MB",
+                "resnet18-submodule_11 -> zircon_res_deq_ReLU_quant_fp_tiled_outer_reduction_workaround_post_conv4_1_RV_E64_MB",
+
+                # Conv4_1 pointwise conv (INNER REDUCTION WORKAROUND)
+                "resnet18-submodule_12 -> zircon_dequant_fp_post_conv4_1_inner_reduction_workaround_RV_E64_MB",
+
+                # Conv4_x
+                "resnet18-submodule_13 -> zircon_deq_ResReLU_fp_post_conv4_x_RV_E64_MB",
+                "resnet18-submodule_14 -> zircon_deq_q_relu_fp_post_conv4_x_RV_E64_MB",
+                "resnet18-submodule_15 -> zircon_deq_ResReLU_quant_fp_post_conv4_x_RV_E64_MB",
+
+                # Conv5_1 strided Conv (INPUT ACTIVATION PADDING WORKAROUND)
+                "resnet18-submodule_16 -> zircon_deq_q_relu_fp_post_conv5_1_RV_E64_MB",
+
+                # Conv5_1 pointwise conv (INNER REDUCTION WORKAROUND, INPUT ACTIVATION PADDING WORKAROUND)
+                "resnet18-submodule_17 -> zircon_dequant_fp_post_conv5_1_inner_reduction_workaround_RV_E64_MB",
+
+                # Conv5_x (K-DIM HOST TILING, INPUT ACTIVATION PADDING WORKAROUND)
+                "resnet18-submodule_18 -> zircon_deq_ResReLU_fp_post_conv5_x_kernel0_RV_E64_MB",
+                "resnet18-submodule_18 -> zircon_deq_ResReLU_fp_post_conv5_x_kernel1_RV_E64_MB",
+                "resnet18-submodule_18 -> zircon_deq_ResReLU_fp_post_conv5_x_kernel2_RV_E64_MB",
+                "resnet18-submodule_18 -> zircon_deq_ResReLU_fp_post_conv5_x_kernel3_RV_E64_MB",
+
+                "resnet18-submodule_19 -> zircon_deq_q_relu_fp_post_conv5_x_kernel0_RV_E64_MB",
+                "resnet18-submodule_19 -> zircon_deq_q_relu_fp_post_conv5_x_kernel1_RV_E64_MB",
+
+                "resnet18-submodule_20 -> zircon_deq_ResReLU_fp_post_conv5_x_kernel0_RV_E64_MB",
+                "resnet18-submodule_20 -> zircon_deq_ResReLU_fp_post_conv5_x_kernel1_RV_E64_MB",
+                "resnet18-submodule_20 -> zircon_deq_ResReLU_fp_post_conv5_x_kernel2_RV_E64_MB",
+                "resnet18-submodule_20 -> zircon_deq_ResReLU_fp_post_conv5_x_kernel3_RV_E64_MB",
+            ]
     configs['mu'] = combine(
         {
             "height": 16, "width": 28, "mu_oc_0": 32, "cols_removed": 12,
@@ -187,16 +266,18 @@ class Configs:
                 "apps/pointwise_mu_io_RV_E64",
                 "apps/pointwise_mu_io_RV_E64_MB"],
         },
-        resnet18_conv2d6,
-        resnet18_conv2d11,
-        resnet18_conv2dfake,
-        resnet18_conv2d16,
-        resnet18_zdqr,
-        resnet18_submod3,
-        resnet18_submod7,
-        resnet18_submod11,
-        resnet18_submod15,
-        resnet18_submod17,
+        # resnet18_conv2d6,
+        # resnet18_conv2d11,
+        # resnet18_conv2dfake,
+        # resnet18_conv2d16,
+#         resnet18_zdqr,
+#         resnet18_submod3,
+#         resnet18_submod7,
+#         resnet18_submod11,
+#         resnet18_submod15,
+#         resnet18_submod17,
+        { 'voyager_cgra_tests_fp': voyager_cgra_tests_fp + voyager_cgra_tests_fp2 },
+        { 'external_mu_tests_fp': external_mu_tests_fp_all },
     )
 
     # ------------------------------------------------------------------------
@@ -221,17 +302,6 @@ class Configs:
     # ------------------------------------------------------------------------
     # pr_aha1
     # ------------------------------------------------------------------------
-    if True:
-            voyager_cgra_tests_fp = [
-                # Standalone quantize layers
-                "resnet18-quantize_default_1::zircon_quant_fp_post_conv2x_RV_E64_MB",
-                "resnet18-quantize_default_3::zircon_quant_fp_post_conv2x_RV_E64_MB",
-                "resnet18-quantize_default_7::zircon_quant_fp_post_conv3x_RV_E64_MB",
-                "resnet18-quantize_default_11::zircon_quant_fp_post_conv4x_RV_E64_MB",
-                "resnet18-quantize_default_15::zircon_quant_fp_post_conv5x_RV_E64_MB",
-            ]
-    
-
     configs['pr_aha1'] = combine(
         {},
         '''
@@ -351,16 +421,6 @@ class Configs:
     # ------------------------------------------------------------------------
     # pr_aha8
     # ------------------------------------------------------------------------
-    if True:
-            voyager_cgra_tests_fp = [
-                # Average pooling layer
-                "resnet18-adaptive_avg_pool2d_default_1::avgpool_layer_fp_RV_E64_MB",
-
-                # Fully connected layer (K-DIM HOST TILING)
-                "resnet18-linear::fully_connected_layer_fp_kernel0_RV_E64_MB",
-                "resnet18-linear::fully_connected_layer_fp_kernel1_RV_E64_MB",
-            ]
-
     configs['pr_aha8'] = combine(
         {},
         '''glb_tests_fp_RV:  # 28m build 12226
@@ -386,7 +446,7 @@ class Configs:
               - apps/rope_pass1_fp_RV
               - apps/rope_pass2_fp_RV
         ''',
-        { 'voyager_cgra_tests_fp': voyager_cgra_tests_fp },
+        { 'voyager_cgra_tests_fp': voyager_cgra_tests_fp2 },
     )
     # ------------------------------------------------------------------------
     # pr_aha9
@@ -970,11 +1030,9 @@ def compare_classes(config):
     # tconfig['foo'] = ['bar']; cconfig['foo'] = ['baz']  # Uncomment to test
 
     err = False
+    print('WARNING (configs.py): Not comparing E64_supported_tests, E64_MB_supported_tests')
     for key in set(list(tconfig.keys()) + list(cconfig.keys())):
-        if key in ["E64_supported_tests","E64_MB_supported_tests"]:
-            print(f'WARNING (configs.py): Not comparing {key}')
-            continue
-
+        if key in ["E64_supported_tests","E64_MB_supported_tests"]: continue
         if key in tconfig: tpretty = json.dumps(tconfig[key], indent=4)
         if key in cconfig: cpretty = json.dumps(cconfig[key], indent=4)
         if key not in tconfig:
@@ -982,16 +1040,34 @@ def compare_classes(config):
         elif key not in cconfig:
             print(f"*** ERROR: Tests({config}) should have {key}= {tpretty}"); err = True
         elif tconfig[key] != cconfig[key]:
+            print(f'*** ERROR: Tests({config})["{key}"] and Configs({config})["{key}"] DIFFER')
+            for app in tconfig[key]:
+                if app not in cconfig[key]:
+                    print(f'  Tests({config})["{key}"] has {app}')
+            for app in cconfig[key]:
+                if app not in tconfig[key]:
+                    print(f'  Config({config})["{key}"] has {app}')
+            print("")
+
+
             tmsg = f'Tests({config})["{key}"] : {tpretty}'
             cmsg = f'Configs({config})["{key}"] : {cpretty}'
-            print(f'*** ERROR: {tmsg}\nDOES NOT EQUAL {cmsg}'); err = True
+#             print(f'*** ERROR: {tmsg}\nDOES NOT EQUAL {cmsg}'); err = True
     if err:
         print(''); return False
     else:
         return True
 
-compare_classes('fast')
+for config in Configs.configs:
+    print(config)
+    compare_classes(config); print("")
+
 exit()
+
+compare_classes('fast')
+
+
+
 
 
 
