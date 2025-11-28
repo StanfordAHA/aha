fad85453f8dedbf168b75a3126c7243e4e09c12b
yuchen-mei <yuchen.mei@outlook.com>
2025-11-28 00:24:46 -0800

    layer norm pass1&2 work with good utilization

--- aha/util/regress.py
+++ aha/util/regress.py
@@ -464,6 +464,7 @@ def test_dense_app(
         "apps/stable_softmax_pass2_fp_RV_E64_MB",
         "apps/stable_softmax_pass3_fp_RV_E64_MB",
         "apps/layer_norm_pass1_fp_RV_E64_MB",
+        "apps/layer_norm_pass2_fp_RV_E64_MB",
         "apps/gelu_pass1_mu_input_fp_RV_E64_MB",
         "apps/gelu_pass2_fp_RV_E64_MB",
     ]
