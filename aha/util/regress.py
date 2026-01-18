018b08542482ab40d336fd474f9d4bd3d83e3444
Michael Oduoza <mcoduoza@gmail.com>
2026-01-17 21:31:47 -0800

    Bump garnet, voyager, regress.py for folder dumping location

--- aha/util/regress.py
+++ aha/util/regress.py
@@ -671,12 +671,19 @@ def test_dense_app(
     time_test = time.time() - start
 
     # HACK: Custom hack for copying folders for chip testing
-    os.system(f"cp -a {app_path}/bin /aha/aha_src")
-    os.system(f"rm -rf /aha/aha_src/{mu_test},,,{orig_test}")
-    os.system(f"mv /aha/aha_src/bin /aha/aha_src/{mu_test},,,{orig_test}")
-    os.system(f"cp -a {voyager_collateral_path}/{mu_test} /aha/voyager_src")
-    os.system(f"rm -rf /aha/voyager_src/{mu_test},,,{orig_test}")
-    os.system(f"mv /aha/voyager_src/{mu_test} /aha/voyager_src/{mu_test},,,{orig_test}")
+    os.system(f"cp -a {app_path}/bin /aha/aha_src3")
+    if mu_test != "inactive":
+        os.system(f"rm -rf /aha/aha_src3/{mu_test},,,{orig_test}")
+        os.system(f"mv /aha/aha_src3/bin /aha/aha_src3/{mu_test},,,{orig_test}")
+        os.system(f"cp -a {voyager_collateral_path}/{mu_test} /aha/voyager_src3")
+        os.system(f"rm -rf /aha/voyager_src3/{mu_test},,,{orig_test}")
+        os.system(f"mv /aha/voyager_src3/{mu_test} /aha/voyager_src3/{mu_test},,,{orig_test}")
+    elif voyager_cgra_test != "":
+        os.system(f"rm -rf /aha/aha_src3/{voyager_cgra_test},,,{orig_test}")
+        os.system(f"mv /aha/aha_src3/bin /aha/aha_src3/{voyager_cgra_test},,,{orig_test}")
+        os.system(f"cp -a {voyager_collateral_path}/{voyager_cgra_test} /aha/voyager_src3")
+        os.system(f"rm -rf /aha/voyager_src3/{voyager_cgra_test},,,{orig_test}")
+        os.system(f"mv /aha/voyager_src3/{voyager_cgra_test} /aha/voyager_src3/{voyager_cgra_test},,,{orig_test}")
 
     active_app_cycles, total_config_cycles, total_write_data_cycles = track_performance()
     return time_compile, time_map, time_test, active_app_cycles, total_config_cycles, total_write_data_cycles
