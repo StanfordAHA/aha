349108941757f82801a540604057d674d8f47296
steveri <steveri@steveri.com>
2026-03-23 10:33:23 -0700

    cleanup and testing and also changed my mind about "1" vs. "0h01"

--- aha/util/regress_info.py
+++ aha/util/regress_info.py
@@ -124,89 +124,95 @@ def appgroups(info1):
               ...
     '''
     def hhmm(nsec):
-        'Turn nsec into hhmm e.g. hhmm(3600)="1h00" or hhmm(120)="   2" '
+        'Turn nsec into hhmm e.g. hhmm(3600)="1h00" or hhmm(120)="0h02" '
         (nmin,nhrs) = (nsec//60,nsec//3600)
-        if nmin < 60: hhmm = f'{nmin:.0f}'                      # "1"  "59"
+        if False: hhmm = f'{nmin:.0f}'                      # "1"  "59"
         else:         hhmm = f'{nmin//60:.0f}h{nmin%60:02.0f}'  # "1h25"
         return f'{hhmm:>6}'
 
-    info2 = []       # Output table goes here
-    grouptotal = 0
+
+    info2 = []   # Output table goes here
+    (grouptotal,ngroupapps) = (0,0)
     blankline = ['','','']
 
     # Easy to count group times if go through list in reverse order, yes?
     info1.reverse()
     for line1 in info1:
-        # print(f'foozy .{line1}.')
+        # Set name and time e.g. if line1=['pointwise', 0] then name='pointwise' and time=0
         (name,time) = ('',0)
         for e in line1:  # This works even if "line" is scalar or 1-element list
-            if not name: name=e
+            if   not name: name=e
             elif not time: time=e
 
         if name.startswith("APP GROUP") or name.startswith("garnet "):
-            if not grouptotal: continue
+            if not ngroupapps: continue  # Skip groups that ran 0 apps
             line2 = f'{hhmm(grouptotal)} {name}'
-            grouptotal = 0
+            (grouptotal,ngroupapps) = (0,0)
         else:
             line2 = f'     {hhmm(time)} {name}'  # Indent app name
             grouptotal += time
+            ngroupapps += 1
         info2.append(line2)
 
         # add blank lines before each "APP GROUP" and garnet "NO Zircon" compilation
         if name.startswith("APP GROUP") or "NO Zircon" in name or "garnet with dense" in name:
             info2.append("")
     info2.reverse()
+    print("FOOO",info2,"------------------")
     return info2
 
-def test_info(info):
-    return  # Remove this line for debugging maybe
+##############################################################################
+# Crude testing support
 
-    from random import randint
-    (t, t1, t2, t3, t4, t5) = 6*[randint(66,6666)]
-    tsuffix='_tsuffix'
-    def t0(): return randint(66,6666)
-    info.append(["garnet (Zircon) with sparse and dense", t])
+DO_TEST1 = False
+DO_TEST2 = False
 
-    info.append([f"APP GROUP sparse_tests[]", 0])
-    info.append(["gen_sparse_bitstreams", t, 0, t, 0])  # Count this as "map" time
-    for test in [ f'sparse-test-{i}' for i in [1,2,3] ]:
-        (t, t1, t2, t3, t4, t5) = 6*[randint(66,6666)]
-        info.append([test + "_glb", t0() + t1 + t2, t0(), t1, t2, t3, t4, t5])
+# Test1: what if app runs in 0 seconds, does it still print the group heading.
+if DO_TEST1:
+  summarize_and_print_info(\
+[
+ ["garnet (Zircon) with sparse and dense",          998 ],
+ ["APP GROUP dense_ml_models[]",                      0 ],
+ ["APP GROUP dense_ml_unit_tests[]",                  0 ],
+ ["pointwise_voyager_full_model",                     0 ],
+ ["APP GROUP sparse_tests[]",                         0 ],
+ ["APP GROUP glb_tests_RV[]",                         0 ],
+ ["APP GROUP glb_tests_fp_RV[]",                      0 ],
+ ["APP GROUP behavioral_mu_tests[]",                  0 ],
+ ["APP GROUP voyager_cgra_tests_fp[]",                0 ],
+ ["APP GROUP external_mu_tests[]",                    0 ],
+ ["APP GROUP external_mu_tests_fp[]",                 0 ],
+])
 
-    for tgroup in ['group1','group2','group3']:
-        info.append([f"APP GROUP {tgroup}[]", 0])
-        tests = [ f'{tgroup}-test{i}' for i in [1,2,3] ] + [f'{tgroup}-buzzfail']
-        for unparsed_name in tests:
-            (t, t1, t2, t3, t4, t5) = 6*[randint(66,6666)]
-            if 'buzzfail' in unparsed_name:
-                # info.append([unparsed_name+tsuffix+" FAIL"])
-                info.append(["*** FAIL ***"])
-                info.append(["*** FAIL " + unparsed_name+tsuffix])
-                info.append(["*** FAIL ***"])
-            else:
-                info.append([unparsed_name+tsuffix, t0()+t1+t2, t0(), t1, t2, t3, t4, t5])
-
-    info.append(["APP GROUP hardcoded_dense_tests[]", 0])
-    info.append(["APP GROUP hardcoded_dense_tests2[]", 0])
-    info.append(["APP GROUP hardcoded_dense_tests3[]", 0])
-
-    info.append(["garnet (NO Zircon) with sparse and dense", t])
-    info.append(["gen_sparse_bitstreams_nz", t, 0, t, 0])  # Count this as "map" time
-    for test in [ f'sparse-test-nz{i}' for i in [1,2,3] ]:
-        (t, t1, t2, t3, t4, t5) = 6*[randint(66,6666)]
-        info.append([test + "_glb", t0() + t1 + t2, t0(), t1, t2, t3, t4, t5])
-
-    for tgroup in ['group4','group5']:
-        info.append([f"APP GROUP {tgroup}[]", 0])
-        tests = [ f'{tgroup}-test{i}' for i in [randint(1,9)]]
-        for test in tests:
-            (t, t1, t2, t3, t4, t5) = 6*[randint(66,6666)]
-            info.append([test + "_glb", t0() + t1 + t2, t0(), t1, t2, t3, t4, t5])
-
-    info.append(["garnet with dense only", t])
-    tests = [ f'{tgroup}-test{i}' for i in [1,2]]
-    for test in tests:
-        (t, t1, t2, t3, t4, t5) = 6*[randint(66,6666)]
-        info.append([test + "_glb dense only", t0() + t1 + t2, t0(), t1, t2, t3, t4, t5])
-
-# summarize_and_print_info(test_info)
+# Test2: what does a normal run look like
+if DO_TEST2:
+  summarize_and_print_info(\
+[
+ ["garnet (Zircon) with sparse and dense",                                         1588 ],
+ ["APP GROUP sparse_tests[]",                                                         0 ],
+ ["gen_sparse_bitstreams",                                                         3018 ],
+ ["vec_elemmul_glb",                                                                143 ],
+ ["mat_vecmul_ij_glb",                                                              135 ],
+ ["mat_sddmm_glb",                                                                  135 ],
+ ["tensor3_mttkrp_glb",                                                             139 ],
+ ["tensor3_ttv_glb",                                                                135 ],
+ ["APP GROUP glb_tests_RV[]",                                                         0 ],
+ ["APP GROUP glb_tests_fp_RV[]",                                                      0 ],
+ ["APP GROUP behavioral_mu_tests[]",                                                  0 ],
+ ["APP GROUP voyager_cgra_tests_fp[]",                                                0 ],
+ ["resnet18-quantize_default_1::zircon_quant_fp_post_conv2x_RV_E64_MB_voyager_standalone_cgra",    2630 ],
+ ["resnet18-quantize_default_3::zircon_quant_fp_post_conv2x_RV_E64_MB_voyager_standalone_cgra",    1363 ],
+ ["resnet18-quantize_default_15::zircon_quant_fp_post_conv5x_RV_E64_MB_voyager_standalone_cgra",   1260 ],
+ ["APP GROUP external_mu_tests[]",                                    0 ],
+ ["APP GROUP external_mu_tests_fp[]",                                 0 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel0_RV_E64_MB_MU_ext",                                                           2972 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel1_RV_E64_MB/zircon_dequantize_relu_fp_post_conv1_kernel1 - SKIP CGRA MAP",        0 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel1_RV_E64_MB/zircon_dequantize_relu_fp_post_conv1_kernel1 - SKIP CGRA PNR",        0 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel1_RV_E64_MB_MU_ext",                                                           1277 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel2_RV_E64_MB/zircon_dequantize_relu_fp_post_conv1_kernel2 - SKIP CGRA MAP",        0 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel2_RV_E64_MB/zircon_dequantize_relu_fp_post_conv1_kernel2 - SKIP CGRA PNR",        0 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel2_RV_E64_MB_MU_ext",                                                           7128 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel3_RV_E64_MB/zircon_dequantize_relu_fp_post_conv1_kernel3 - SKIP CGRA MAP",        0 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel3_RV_E64_MB/zircon_dequantize_relu_fp_post_conv1_kernel3 - SKIP CGRA PNR",        0 ],
+ ["resnet18-submodule -> zircon_dequantize_relu_fp_post_conv1_kernel3_RV_E64_MB_MU_ext",                                                           4254 ],
+])
